25d676e1bb8a6c7b9837cd10117445fae4db3935
Dennis de Vries <dennisdevries2511@hotmail.com>
2021-08-20 14:09:25 +0200

    Add timing listing

---
+++ components/timing/Listing.vue
@@ -0,0 +1,74 @@
+<template>
+  <!--
+    There is a bug in the combination of SSR and v-data-table which causes
+    an error stating that the virtual DOM does not match server-rendered content.
+    It can be resolved, but not in the time that is left for the assignment.
+  -->
+  <client-only>
+    <v-card>
+      <v-card-title>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search by user"
+          single-line
+          hide-details
+        />
+      </v-card-title>
+
+      <v-data-table
+        :items="items"
+        :headers="headers"
+        :search="search"
+        must-sort
+        sort-by="laptime"
+      />
+    </v-card>
+  </client-only>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import { DataTableHeader, DataTableItemProps } from 'vuetify'
+import { ITiming, IUser, TTimingList, TUserList } from '~/types'
+
+export default defineComponent({
+  name: 'TimingListing',
+  data: () => ({
+    search: '',
+    headers: [
+      { text: 'Laptime (sec)', value: 'laptime', sortable: true },
+      { text: 'User', value: 'user', filterable: true },
+      { text: 'Datetime', value: 'datetime', sortable: true },
+    ] as Array<DataTableHeader>
+  }),
+  computed: {
+    timings(): TTimingList {
+      return this.$store.state.timings.timings
+    },
+    users(): TUserList {
+      return this.$store.state.users.users
+    },
+    items(): Array<DataTableItemProps> {
+      return this.timings.map((timing: ITiming) => {
+        const user = this.users.find((user: IUser) => user.id === timing.userId)
+
+        let fullname = user.first_name
+
+        if (user.last_name_prefix)
+          fullname = `${fullname} ${user.last_name_prefix}`
+
+        fullname = `${fullname} ${user.last_name}`
+
+        return {
+          ...timing,
+          user: fullname
+        }
+      })
+    },
+  },
+  async beforeMount() {
+    await this.$store.dispatch('users/fetchUsers')
+  },
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
-    <h1>Overview</h1>
+    <div style="width: 100%;">
+        <TimingListing />
+    </div>
 </template>
 
 <script>
 export default {
     name: 'Overview',
+    components: {
+        'TimingListing': () => import('@/components/timing/Listing.vue')
+    },
 };
 </script>
