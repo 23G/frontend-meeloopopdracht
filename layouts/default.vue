d821d0f8244c91836e09c7b65d74119833448ecb
Dennis de Vries <dennisdevries2511@hotmail.com>
2021-08-20 09:38:10 +0200

    Set default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,62 @@
 <template>
-    <div>
-        <nuxt-link to="/" class="text-xl font-bold">23GP</nuxt-link>
-        <div class="flex">
-            <nuxt-link to="/">Overview</nuxt-link>
-            <nuxt-link to="/leaderboard">Leaderboard</nuxt-link>
-        </div>
-        <Nuxt />
-    </div>
+    <v-app dark>
+        <v-app-bar
+            fixed
+            app
+            clipped-left
+        >
+            <NuxtLink to="/">
+                <Logo23GLogo />
+            </NuxtLink>
+        </v-app-bar>
+
+        <v-navigation-drawer permanent app clipped>
+            <v-list dense nav>
+                <v-list-item v-for="(item, i) in items" :key="`nav-item-${i}`" :to="item.to" nuxt>
+                    <v-list-item-icon>
+                        <v-icon>
+                            {{ item.icon }}
+                        </v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                        <v-list-item-title v-text="item.title" />
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-main>
+            <v-container fluid class="fill-height align-start">
+                <Nuxt />
+            </v-container>
+        </v-main>
+
+        <v-footer app>
+            <span>&copy; {{ new Date().getFullYear() }}</span>
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
 export default {
     name: 'LayoutDefault',
-};
+    components: {
+        'Logo23GLogo': () => import('~/components/logo/23GLogo.vue')
+    },
+    data: () => ({
+        fixed: true,
+        items: [
+            {
+                icon: 'mdi-view-list-outline',
+                title: 'Overview',
+                to: '/'
+            },
+            {
+                icon: 'mdi-timetable',
+                title: 'Leaderboard',
+                to: '/leaderboard'
+            },
+        ],
+    }),
+}
 </script>
