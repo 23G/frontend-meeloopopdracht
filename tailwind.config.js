6d700dfba266ab0f05f4e4d0d8692e54088fb29d
yhanssens <youri@23g.nl>
2022-02-10 15:48:11 +0100

    Init nuxt 3

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ['./**/*.{ts,vue,scss}'],
+    theme: {
+        extend: {
+            colors: {
+                dark: '#0f0e14',
+                primary: '#e51c2a',
+            },
+        },
+    },
+};
